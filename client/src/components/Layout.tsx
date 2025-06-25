d7216c611a5eb2a89801de01d9cdfd7649681876
nguyenhungAAA <quanghunglenguyen@gmail.com>
2025-06-25 14:30:25 +0700

    feat: add layout modal for session expiration notification and implement useApi hook for API calls

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -3,23 +3,45 @@ import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import MobileMenu from './MobileMenu';
+import Modal from '@/components/modal/ModalNotification';
+import useModal from '@/hooks/useModal';
+
 
 const Layout: React.FC = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const location = useLocation();
+  const { isOpen, openModal, closeModal } = useModal();
 
   const toggleMobileMenu = () => {
     setMobileMenuOpen(!mobileMenuOpen);
   };
 
+  const token = localStorage.getItem('token');
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location.key])
+
+  useEffect(() => {
+    if (!token) {
+      openModal();
+    }
+  }, [token])
 
   return (
 
     <div className="flex flex-col min-h-screen">
       <Header toggleMobileMenu={toggleMobileMenu} />
+      <Modal
+        isOpen={isOpen}
+        onClose={closeModal}
+        title="Thông báo"
+        description="Phiên đăng nhập đã hết, vui lòng đăng nhập lại để có thể sử dụng nhiều dịch vụ hơn"
+        confirmMessage="Đăng nhập ngay"
+        confirmUrl={() => {
+          window.location.href = '/login';
+        }}
+      />
       <MobileMenu isOpen={mobileMenuOpen} onClose={() => setMobileMenuOpen(false)} />
       <main className="flex-grow">
         <Outlet />
